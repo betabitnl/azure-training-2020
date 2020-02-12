3ad39227365565a106a0780d6d213391708a4299
Rick van den Bosch <rickvdbosch@outlook.com>
2020-02-12 18:13:17 +0100

    Console app KeyVault - 'Old' structure

---
+++ Betabit.Training.Azure.KeyVault.ConsoleApp.Old/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Betabit.Training.Azure.Common;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace Betabit.Training.Azure.KeyVault.Old
+{
+    class Program
+    {
+        #region Constants
+
+        const string SECRET_NAME = "<YOUR-SECRET-NAME>";
+        const string VAULT_NAME = "<YOUR-VAULT-NAME>";
+
+        #endregion
+
+        static async Task Main(string[] args)
+        {
+            var tokenProvider = new AzureServiceTokenProvider();
+            var callback = new KeyVaultClient.AuthenticationCallback(tokenProvider.KeyVaultTokenCallback);
+            var client = new KeyVaultClient(callback);
+
+            var secret = await client.GetSecretAsync(KeyVaultHelper.GetVaultUrl(VAULT_NAME), SECRET_NAME);
+
+            Console.WriteLine($"The secret value I retrieved from Key Vault: {secret.Value}!");
+        }
+    }
+}
