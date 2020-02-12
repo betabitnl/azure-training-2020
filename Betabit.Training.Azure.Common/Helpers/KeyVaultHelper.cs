edb743bf46ff1f1b94cb5eaf9ca0fef4bdd9dd28
Rick van den Bosch <rickvdbosch@outlook.com>
2020-02-12 18:13:56 +0100

    Azure common project

---
+++ Betabit.Training.Azure.Common/Helpers/KeyVaultHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Betabit.Training.Azure.Common
+{
+    public static class KeyVaultHelper
+    {
+        /// <summary>
+        /// Returns a URI to an Azure Key Vault based on the name of the vault.
+        /// </summary>
+        /// <param name="vaultName">Name of the Key Vault.</param>
+        /// <returns>A <see cref="Uri"/> for the specified <paramref name="vaultName"/>.</returns>
+        public static Uri GetVaultUri(string vaultName)
+        {
+            return new Uri(GetVaultUrl(vaultName));
+        }
+
+        /// <summary>
+        /// Returns a URL to an Azure Key Vault based on the name of the vault.
+        /// </summary>
+        /// <param name="vaultName">Name of the Key Vault.</param>
+        /// <returns>A URL for the specified <paramref name="vaultName"/>.</returns>
+        public static string GetVaultUrl(string vaultName)
+        {
+            return $"https://{vaultName}.vault.azure.net/";
+        }
+    }
+}
