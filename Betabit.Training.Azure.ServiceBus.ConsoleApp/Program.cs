7c70645f2e05102e9ebb129fc0db56bda2c96372
Rick van den Bosch <rickvdbosch@outlook.com>
2020-02-12 18:14:19 +0100

    Service Bus 'message pump'

---
+++ Betabit.Training.Azure.ServiceBus.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Betabit.Training.Azure.ServiceBus.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new QueueClient(Secrets.CONNECTIONSTRING, Secrets.QUEUENAME);
+
+            Console.WriteLine("Enter 'q' (without quotes) and hit enter to quit");
+            string messageBody = GetInput();
+
+            while (!messageBody.Equals("q", StringComparison.OrdinalIgnoreCase))
+            {
+                var body = Encoding.UTF8.GetBytes(messageBody);
+                await client.SendAsync(new Message(body));
+                messageBody = GetInput();
+            }
+        }
+
+        #region Private methods
+
+        private static string GetInput()
+        {
+            Console.WriteLine("What would you like to send?");
+            var messageBody = Console.ReadLine();
+            return messageBody;
+        }
+
+        #endregion
+    }
+}
---
+++ Betabit.Training.Azure.ServiceBus.ConsoleApp/Secrets.cs
@@ -0,0 +1,9 @@
+﻿namespace Betabit.Training.Azure.ServiceBus.ConsoleApp
+{
+    internal static class Secrets
+    {
+        public const string CONNECTIONSTRING = "<YOUR-SERVICEBUS-CONNECTIONSTRING>";
+
+        public const string QUEUENAME = "<YOUR-QUEUE-NAME>";
+    }
+}
